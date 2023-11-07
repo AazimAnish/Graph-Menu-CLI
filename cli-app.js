ba8f355639418a42df7cf1a912b46187c40f5e02
AazimAnish <aazimanish1@gmail.com>
2023-11-07 05:54:19 +0530

    converted to graph

--- cli-app.js
+++ cli-app.js
@@ -4,219 +4,122 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let currentOption = 'main';
-const userData = {}; // Store user data
-const registeredUsers = []; // Store registered users
-
-function showMenu() {
-  switch (currentOption) {
-    case 'main':
-      console.log('Welcome to the interactive system!');
-      console.log('Choose an option:');
-      console.log('1. Register');
-      console.log('2. Track Delivery');
-      console.log('3. Contact Support');
-      console.log('4. Admin');
-      console.log('5. Exit');
-
-      rl.question('Enter your choice: ', choice => {
-        switch (choice) {
-          case '1':
-            currentOption = 'register';
-            showRegisterMenu();
-            break;
-          case '2':
-            currentOption = 'trackDelivery';
-            showTrackDeliveryMenu();
-            break;
-          case '3':
-            showContactSupport();
-            break;
-          case '4':
-            currentOption = 'admin';
-            showAdminMenu();
-            break;
-          case '5':
-            console.log('Thank you for using our system!');
-            rl.close();
-            break;
-          default:
-            console.log('Invalid option. Please choose a valid option.');
-            showMenu();
-        }
-      });
-      break;
-    case 'register':
-      showRegisterMenu();
-      break;
-    case 'trackDelivery':
-      showTrackDeliveryMenu();
-      break;
-    case 'admin':
-      showAdminMenu();
-      break;
+class Graph {
+  constructor() {
+    this.nodes = new Map();
   }
-}
 
-function showRegisterMenu() {
-  console.log('Registration');
+  registerPerson(firstName, lastName, dob) {
+    // Using a combination of first and last name as the key to handle duplicates
+    this.nodes.set(`${firstName} ${lastName}`, {
+      firstName,
+      lastName,
+      dob,
+    });
+    console.log("Person registered.");
+    this.showMenu(); // Go back to the menu after registering
+  }
 
-  rl.question('Enter first name: ', firstName => {
-    userData['First Name'] = firstName;
+  contactUs() {
+    console.log("Name: Aazim Anish, Email: aazimanish1@gmail.com, Phone: 9562037068");
+    this.showMenu(); // Go back to the menu after showing contact details
+  }
 
-    rl.question('Enter last name: ', lastName => {
-      userData['Last Name'] = lastName;
-
-      rl.question('Enter date of birth: ', dateOfBirth => {
-        userData['Date of Birth'] = dateOfBirth;
-
-        registeredUsers.push({ ...userData });
-
-        console.log('Registration successful.');
-        console.log(`Name: ${userData['First Name']} ${userData['Last Name']}`);
-        console.log(`Date of birth: ${userData['Date of Birth']}`);
-
-        rl.question('Back to main menu? (y/n): ', backToMain => {
-          if (backToMain === 'y') {
-            currentOption = 'main';
-            showMenu();
+  addShipment() {
+    rl.question("Enter Tracking ID (8 digits): ", trackId => {
+      if (trackId.length === 8) {
+        rl.question("Enter Phone Number (10 digits): ", phoneNo => {
+          if (phoneNo.length === 10 && /^\d{10}$/.test(phoneNo)) {
+            console.log("Your shipment is currently in Kochi and will be out for delivery within 2 days.");
+            this.showMenu();
           } else {
-            showRegisterMenu();
+            console.log("Invalid phone number. It must be 10 digits.");
+            this.addShipment(); // Retry the process
           }
+        });
+      } else {
+        console.log("Invalid Tracking ID. It must be 8 digits.");
+        this.addShipment(); // Retry the process
+      }
+    });
+  }
+
+
+  adminSearch(name) {
+    let found = false;
+    this.nodes.forEach((value, key) => {
+      if (key.includes(name)) {
+        console.log(`\nDetails for ${key}:`);
+        console.log(`First Name: ${value.firstName}`);
+        console.log(`Last Name: ${value.lastName}`);
+        console.log(`DOB: ${value.dob}`);
+        found = true;
+      }
+    });
+
+    if (!found) {
+      console.log("No person found with that name.");
+    }
+
+    this.showMenu(); // Go back to the menu after searching
+  }
+
+  showMenu() {
+    console.log("\nMenu:");
+    console.log("1. Register Person");
+    console.log("2. Contact Us");
+    console.log("3. Track Delivery");
+    console.log("4. Admin Search");
+    console.log("5. Exit");
+
+    rl.question("Choose an option: ", (option) => {
+      switch (option) {
+        case "1":
+          this.promptForPersonDetails();
+          break;
+        case "2":
+          this.contactUs();
+          break;
+        case "3":
+          this.promptForShipmentDetails();
+          break;
+        case "4":
+          this.promptForAdminSearch();
+          break;
+        case "5":
+          rl.close();
+          break;
+        default:
+          console.log("Invalid option. Please choose a valid option.");
+          this.showMenu();
+      }
+    });
+  }
+
+  promptForPersonDetails() {
+    rl.question("Enter first name: ", firstName => {
+      rl.question("Enter last name: ", lastName => {
+        rl.question("Enter date of birth: ", dob => {
+          this.registerPerson(firstName, lastName, dob);
         });
       });
     });
-  });
-}
+  }
 
-function showTrackDeliveryMenu() {
-  console.log('Track Delivery');
-
-  console.log('Choose an option:');
-  console.log('1. Track by ID');
-  console.log('2. Track by phone number');
-  console.log('3. Back to main menu');
-
-  rl.question('Enter your choice: ', choice => {
-    switch (choice) {
-      case '1':
-        trackById();
-        break;
-      case '2':
-        trackByPhoneNumber();
-        break;
-      case '3':
-        currentOption = 'main';
-        showMenu();
-        break;
-      default:
-        console.log('Invalid option. Please choose a valid option.');
-        showTrackDeliveryMenu();
-    }
-  });
-}
-
-function trackById() {
-  console.log('Track by ID');
-
-  rl.question('Enter tracking ID: ', trackingId => {
-    console.log(`Tracking ID ${trackingId} verified.`);
-    console.log('Your product is currently in Kochi and will be delivered within 2 days.');
-
-    rl.question('Back to track delivery menu? (y/n): ', backToTrackDelivery => {
-      if (backToTrackDelivery === 'y') {
-        showTrackDeliveryMenu();
-      } else {
-        showMenu();
-      }
-    });
-  });
-}
-
-function trackByPhoneNumber() {
-  console.log('Track by phone number');
-
-  rl.question('Enter phone number: ', phoneNumber => {
-    console.log(`Phone number ${phoneNumber} verified.`);
-    console.log('Your product is currently in Kochi and will be delivered within 2 days.');
-
-    rl.question('Back to track delivery menu? (y/n): ', backToTrackDelivery => {
-      if (backToTrackDelivery === 'y') {
-        showTrackDeliveryMenu();
-      } else {
-        showMenu();
-      }
-    });
-  });
-}
-
-function showContactSupport() {
-  console.log('Contact Support');
-  console.log('Admin details:');
-  console.log('Name: Aazim Anish');
-  console.log('Email: aazimanish1@gmail.com');
-  console.log('Phone number: 9562037068');
-
-  rl.question('Back to main menu? (y/n): ', backToMain => {
-    if (backToMain === 'y') {
-      currentOption = 'main';
-      showMenu();
-    } else {
-      // Continue within the current submenu
-    }
-  });
-}
-
-function showAdminMenu() {
-  console.log('Admin Menu');
-  console.log('1. Search Registered Users');
-  console.log('2. Back to main menu');
-
-  rl.question('Enter your choice: ', choice => {
-    switch (choice) {
-      case '1':
-        searchRegisteredUsers();
-        break;
-      case '2':
-        currentOption = 'main';
-        showMenu();
-        break;
-      default:
-        console.log('Invalid option. Please choose a valid option.');
-        showAdminMenu();
-    }
-  });
-}
-
-function searchRegisteredUsers() {
-  console.log('Search Registered Users');
-  rl.question('Enter a search query (first name, last name): ', searchQuery => {
-    const matchingUsers = registeredUsers.filter(user => {
-      return (
-        user['First Name'].toLowerCase().includes(searchQuery.toLowerCase()) ||
-        user['Last Name'].toLowerCase().includes(searchQuery.toLowerCase())
-      );
-    });
-
-    if (matchingUsers.length > 0) {
-      console.log('Matching Users:');
-      matchingUsers.forEach((user, index) => {
-        console.log(`User ${index + 1}:`);
-        console.log(`Name: ${user['First Name']} ${user['Last Name']}`);
-        console.log(`Date of Birth: ${user['Date of Birth']}`);
+  promptForShipmentDetails() {
+      rl.question("Enter shipment ID: ", shipmentId => {
+        this.addShipment(shipmentId);
       });
-    } else {
-      console.log('No matching users found.');
-    }
+  }
 
-    rl.question('Back to admin menu? (y/n): ', backToAdmin => {
-      if (backToAdmin === 'y') {
-        showAdminMenu();
-      } else {
-        showMenu();
-      }
+  promptForAdminSearch() {
+    rl.question("Enter first or last name to search: ", name => {
+      this.adminSearch(name);
     });
-  });
+  }
 }
 
-showMenu();
+const graph = new Graph();
+
+console.log("Welcome to the interactive system!");
+graph.showMenu();
